55703a7f469062300c1db630910cab818eca7ee3
etorth <anhonghe@gmail.com>
2025-09-28 11:30:31 -0400

    f

---
+++ src/main.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+import sys
+from PySide6.QtWidgets import QApplication
+from qdmainwindow import QD_MainWindow
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    mainWindow = QD_MainWindow()
+    mainWindow.show()
+
+    sys.exit(app.exec())
